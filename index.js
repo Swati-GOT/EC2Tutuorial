c0695e16d91b439ff92be87e13719630bec09c9e
Swati Prajapati <swati.prajapati@neosoftmail.com>
2022-08-19 18:42:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Hapi = require('@hapi/hapi');
+const Infiniteloop = require('infinite-loop');
 const init = async () => {
   const server = Hapi.server({
       port: 8080,
@@ -12,10 +13,30 @@ const init = async () => {
       return 'API is running!';
   }
  });
+
+ server.route({
+  method: 'GET',
+  path: '/hello',
+handler: (request, h) => {
+  return 'API is running at helloworld!';
+}
+});
+
 2
  await server.start();
  console.log('Server running on %s', server.info.uri);
+//  var loop = new Infiniteloop();
+//  //use loop.add to add a function
+//  //fisrt argument should be the fn, the rest is the fn's arguments
+//  loop.add(say, 'Looping', ' Forever!');
+//  //make it run
+//  loop.run();
 };
+
+function say(word1, word2){
+  console.log(word1 + word2);
+};
+
 process.on('unhandledRejection', (err) => {
  console.log(err);
  process.exit(1);
